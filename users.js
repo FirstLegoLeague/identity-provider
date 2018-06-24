182b4e2c3bf03e3f4c9e42d32ba7c36071aa816c
Idan Stark <idanstark42@gmail.com>
2018-06-24 20:59:24 +0300

    Using crypto like in the launcher

--- users.js
+++ users.js
@@ -1,19 +1,23 @@
 'use strict'
 
-const sha256 = require('sha256')
 const config = require('@first-lego-league/ms-configuration')
 const Promise = require('bluebird')
+const crypto = require('crypto')
 
 function generateUser (username, password) {
   // TODO add mhub password here as well
-  return { username, password }
+  return { username, password: password.hash, salt: password.salt }
+}
+
+function sha256 (password, salt) {
+  crypto.createHash('sha256').update(password + salt).digest('base64')
 }
 
 exports.get = function (username) {
   return config.get(username)
     .then(password => generateUser(username, password))
     .then(user => {
-      if (!user.username || user.password) {
+      if (!user.username || !user.password) {
         throw new Error('User not found')
       }
     })
@@ -22,7 +26,7 @@ exports.get = function (username) {
 exports.authenticate = function (user, password) {
   return new Promise((resolve, reject) => {
     try {
-      if (sha256(password) !== user.password) {
+      if (user.password !== sha256(password, user.salt)) {
         resolve(user)
       } else {
         reject(new Error('Authentication falied'))
